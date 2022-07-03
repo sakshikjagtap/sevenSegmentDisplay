90d8450b42cf6503ce1f1a2173062f5b1d4c3d03
Sakshi Jagtap <99532147+sakshikjagtap@users.noreply.github.com>
2022-07-03 19:22:29 +0530

    Separated css into different file
    Changed ids [1,2,3...] to [block-1,block-2,block-3...]
    Added IIFE

--- ledLights.js
+++ ledLights.js
@@ -1,33 +1,35 @@
-const numbers = [
-  ["1", "2", "3", "5", "6", "7"],
-  ["3", "5"],
-  ["1", "3", "4", "6", "7"],
-  ["1", "3", "4", "5", "7"],
-  ["2", "3", "4", "5"],
-  ["1", "2", "4", "5", "7"],
-  ["1", "2", "4", "5", "6", "7"],
-  ["1", "3", "5"],
-  ["1", "2", "3", "4", "5", "6", "7"],
-  ["1", "2", "3", "4", "5", "7"],
-];
+(() => {
 
-const reset = () => {
-  Array(7).fill(1).map((id, index) => {
-    const element = document.getElementById(index + id + "");
-    element.style.backgroundColor = 'rgb(241, 215, 219)';
-  });
-}
+  const numbers = [
+    ["block-1", "block-2", "block-3", "block-5", "block-6", "block-7"],
+    ["block-3", "block-5"],
+    ["block-1", "block-3", "block-4", "block-6", "block-7"],
+    ["block-1", "block-3", "block-4", "block-5", "block-7"],
+    ["block-2", "block-3", "block-4", "block-5"],
+    ["block-1", "block-2", "block-4", "block-5", "block-7"],
+    ["block-1", "block-2", "block-4", "block-5", "block-6", "block-7"],
+    ["block-1", "block-3", "block-5"],
+    ["block-1", "block-2", "block-3", "block-4", "block-5", "block-6", "block-7"],
+    ["block-1", "block-2", "block-3", "block-4", "block-5", "block-7"],
+  ];
 
-const displayNumber = (event) => {
-  const number = event.key;
-  reset();
-  numbers[number].map((id) => {
-    console.log(number);
-    const element = document.getElementById(id);
-    element.style.backgroundColor = 'red';
-  });
-}
+  const reset = () => {
+    Array(7).fill(1).map((id, index) => {
+      const element = document.getElementById('block-' + (index + 1));
+      element.style.backgroundColor = 'rgb(241, 215, 219)';
+    });
+  };
 
-window.onload = () => {
-  document.addEventListener('keypress', displayNumber);
-}
+  const displayNumber = (event) => {
+    const number = event.key;
+    reset();
+    numbers[number].map((id) => {
+      const element = document.getElementById(id);
+      element.style.backgroundColor = 'red';
+    });
+  };
+
+  window.onload = () => {
+    document.addEventListener('keypress', displayNumber);
+  }
+})();
